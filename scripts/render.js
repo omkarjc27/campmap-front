19d75055f22ad5f7e7cd16d36a92994984e73f36
Omkar Chalke <omkarchalke@protonmail.com>
2021-04-30 19:30:07 +0530

    closing in to final draft

--- scripts/render.js
+++ scripts/render.js
@@ -1,6 +1,6 @@
 obj = campus["watumull"]
 var navdiv = document.getElementById("sidebar");
-var views = '<button class="w3-bar-item" style="font-size: 4vh;background-color: rgba(12,14,14,0.95);"></button>'
+var views = '<button class="w3-xxlarge w3-cancel" style="text-align:right;" onclick="w3_close()">&times;</button><br>'
 i = 0
 for (var key in obj) {
 	if (obj.hasOwnProperty(key)) {
@@ -10,11 +10,13 @@ for (var key in obj) {
 	}
 }
 views += '<hr>'
-views += '<a href="entry.html#login" class="w3-bar-item w3-button"  style="font-size: 3vh;">Login</a>\n'
-views += '<a href="entry.html#contact" class="w3-bar-item w3-button"  style="font-size: 3vh;">Contact Us</a>\n'
-views += '<a href="entry.html#faq" class="w3-bar-item w3-button"  style="font-size: 3vh;">FAQs</a>\n'
+views += '<a href="entry.html#login" class="w3-bar-item w3-button w3-round"  style="font-size: 3vh;">Login</a>\n'
+views += '<a href="entry.html#contact" class="w3-bar-item w3-button w3-round"  style="font-size: 3vh;">Contact Us</a>\n'
+views += '<a href="entry.html#faq" class="w3-bar-item w3-button w3-round"  style="font-size: 3vh;">FAQs</a>\n'
 navdiv.innerHTML=views
 function render(college,view,status) {
+		status = JSON.parse(status)
+		if (Object.keys(status).length==0){var status = {"a":"b"}}
 		var viewdiv = document.getElementById("view-name");
 		viewdiv.innerHTML = "<b>"+view+"</b>"
 		var mapdiv = document.getElementById("map");
@@ -24,12 +26,13 @@ function render(college,view,status) {
 		var right = 0
 		for (var i = 0; i < rooms.length; i++) {
 			if (rooms[i]["type"]=="room"){
-				map += '<div class="'+rooms[i]["type"]+'" style="width:'+(rooms[i]["size"][0])+'vh;max-width:'+(rooms[i]["size"][0])+'vh;height:'+(rooms[i]["size"][1])+'vh;max-height:'+(rooms[i]["size"][1])+'vh;left:'+(rooms[i]["location"][0])+'vh;top:'+(rooms[i]["location"][1])+'vh;font-size:2.5vh;" onclick=alert("Hello")>'+rooms[i]["name"]
-					if (status.hasOwnProperty(rooms[i]["name"])){
-						map+=': <span class="w3-text-red">Occupied ('+status[rooms[i]["name"]]+')</span></div>'
-					}else{
-						map+=': <span class="w3-text-green">Vacant </span></div>'
-					}
+				map+= '<div class="'+rooms[i]["type"]+'" style="width:'+(rooms[i]["size"][0])+'vh;max-width:'+(rooms[i]["size"][0])+'vh;height:'+(rooms[i]["size"][1])+'vh;max-height:'+(rooms[i]["size"][1])+'vh;left:'+(rooms[i]["location"][0])+'vh;top:'+(rooms[i]["location"][1])+'vh;font-size:2vh;" >'+rooms[i]["name"]
+				if (status.hasOwnProperty((rooms[i]["name"]).split(' ').join('\u00a0'))){
+					map+=': <span class="w3-text-red" class="font-size:2vh">Occupied ('+status[(rooms[i]["name"]).split(' ').join('\u00a0')]+')</span>'
+				}else{
+					map+=': <span class="w3-text-green" class="font-size:2vh">Vacant </span>'
+				}
+				map+= '<button class="edit-btn" onclick=statusupdate("'+college+'\",\"'+view+'\",\"'+String(rooms[i]["name"]).split(' ').join('&nbsp;')+'\") ><i class="fa fa-pencil"></i></button></div>'
 			}
 			else{
 				map += '<div class="'+rooms[i]["type"]+'" style="width:'+(rooms[i]["size"][0])+'vh;max-width:'+(rooms[i]["size"][0])+'vh;height:'+(rooms[i]["size"][1])+'vh;max-height:'+(rooms[i]["size"][1])+'vh;left:'+(rooms[i]["location"][0])+'vh;top:'+(rooms[i]["location"][1])+'vh;font-size:2vh;">'+rooms[i]["name"]+'</div>'
@@ -48,9 +51,34 @@ function render(college,view,status) {
 }
 function w3_open(){document.getElementById("sidebar").style.display = "block";}
 function w3_close(){document.getElementById("sidebar").style.display = "none";}
-
+function statusupdate(campus_name,view,room){
+	if(window.localStorage.getItem("CampMap_User_ID")==null){
+		document.getElementById("purpose_e").style.display = "block";		
+	}
+	else{
+		document.getElementById('college_field').value=campus_name;
+		document.getElementById('view_field').value=view;
+		document.getElementById('room_field').value=room;
+		document.getElementById("purpose").style.display = "block";
+	}
+}
+function close_stat(){
+	document.getElementById("purpose_e").style.display = "none";
+	document.getElementById("purpose").style.display = "none";
+}
+function status_form(){
+	var purp=document.getElementById('purpose_field').value;
+	var college=document.getElementById('college_field').value;
+	var view=document.getElementById('view_field').value;
+	var room=document.getElementById('room_field').value;
+	Update_Status(college,view,room,purp)
+	close_stat()
+}
 function Update_Status(campus_name,view,room,purpose){
+	var mapdiv = document.getElementById("map");
+	mapdiv.innerHTML='<center><i class="fa fa-spinner fa-spin" style="font-size:10vh;margin-top: 35vh;"></i></center>'
 	var token = window.localStorage.getItem("CampMap_User_ID");
+	
 	var ourRequest = new XMLHttpRequest();
 	ourRequest.open('POST', 'https://campus-map-api.herokuapp.com/Update/');
 	ourRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -61,7 +89,7 @@ function Update_Status(campus_name,view,room,purpose){
 			if (data=="BadLogin"){
 				window.location.href = "entry.html#login"
 			} else if (data==true){
-				View_Status("watumull","GndFloor")
+				View_Status(campus_name,view)
 			}
 		} else {
 			alert('Server Error! Our Team is working on fixing it.')
@@ -79,15 +107,12 @@ function View_Status(campus_name,view){
 	ourRequest.send(JSON.stringify({ "campus": campus_name, "view": view}));
 	ourRequest.onload = function() {
 		if (ourRequest.status >= 200 && ourRequest.status < 400) {
-			status = JSON.parse(ourRequest.responseText)
-			console.log(Object.keys(status).length)
-			if (Object.keys(status).length==0){var status = {"a":"b"}}
-			render(campus_name,view,status)
+			render(campus_name,view,ourRequest.responseText)
 		} else {
 			alert('Server Error! Our Team is working on fixing it.')
 		}
 	}
 
 }
-Update_Status("watumull","GndFloor","FE EXTC Classroom","work")
-//View_Status("watumull","GndFloor")
+//Update_Status("watumull","GndFloor","FE EXTC Classroom","work")
+View_Status("watumull","GndFloor")
